aa5a54ebfb651cedf10c38d806392b7a76eb382e
Jason Stanley <jason.w.stanley@leidos.com>
2021-09-02 13:07:29 -0400

    update ui

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Fib</h1>
+          <h1 className="App-title">FIB Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other</Link>
         </header>
